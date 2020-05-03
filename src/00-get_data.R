2ad8dedf7f0334451b153d00fd4b70c7f3e39f8c
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2020-05-03 18:53:56 -0500

    Add comments on processing scripts

--- src/00-get_data.R
+++ src/00-get_data.R
@@ -1,6 +1,10 @@
 library(qualtRics)
+
+#Get Survey from Quatrics with API
 seas_survey_id = 'SV_ekZqvDRASUWJnnv'
 df_seas = fetch_survey(surveyID = seas_survey_id,
                     force_request = TRUE,
                     time_zone = "America/Chicago")
+
+#Save as RDS to keep column labels
 saveRDS(df_seas, "raw/seas_one.rds")
--- src/02-get_col_labels.R
+++ src/02-get_col_labels.R
@@ -1,12 +1,14 @@
-# Store labels
+# Get column labels that match columns in cleaned df
 
+#Load raw and cleaned data
 df_raw = readRDS("./raw/qualtrics_seas_one.rds")
 df_clean = read.csv("./data/main/df_seas_one_clean.csv")
 
+# Get label for each column from raw data
 labels = vapply(df_raw, 
                 function(x){attr(x, "label")},
                 character(1))
 
-#Keeponly labels in df_seas_one_clean.csv
+# Subset labels to keep only the ones for columns in clean data
 labels = labels[names(labels) %in% colnames(df_seas)]
 saveRDS(labels, "data/labels/seas_one_labels.rds")
